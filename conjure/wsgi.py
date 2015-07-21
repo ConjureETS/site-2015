148fa2ae9a291f4067adb796d61fab9fc80cc859
Émile Filteau <emile.filteau@gmail.com>
2015-07-21 15:52:28 -0400

    Added all templates, fixed medias with dj_static and added requirements.txt

--- conjure/wsgi.py
+++ conjure/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling, MediaCling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conjure.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))

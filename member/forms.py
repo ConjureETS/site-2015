99903c9b684a8b7aeb9dfbf053042f1f661d8346
Émile Filteau <emile.filteau@gmail.com>
2015-07-21 16:28:55 -0400

    Fixed stuff

--- member/forms.py
+++ member/forms.py
@@ -5,3 +5,4 @@ class MemberAdminForm(forms.ModelForm):
     photo = forms.ImageField(required=False)
     class Meta:
         model = models.Member
+        fields = ['first_name', 'last_name', 'email', 'photo', 'is_admin', 'position']
